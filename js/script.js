a02342a4398d057d765bee7bba40528815181674
Owen Bossola <owen.bossola@gmail.com>
2012-10-21 23:36:39 -0400

    more organization; simple info button

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 /* Author: 
 
+    Owen Bossola
+
 */
 
+(function($) {
 
+	var info = $('#info');
 
+	$('.info')
+		.click(function() {
+			if (info.hasClass('slidein')) {
+				info.removeClass('slidein');
+				info.addClass('slideout');
+			}
+			else {
+				info.removeClass('slideout');
+				info.addClass('slidein');
+			}
+		});
 
+	$('#info')
+		.mouseleave(function() {
+			$(this)
+				.removeClass('slidein')
+				.addClass('slideout');
+		});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})(window.jQuery);
